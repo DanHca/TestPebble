e3094e29c50877fc17545771324e2b2f49689670
DanHca <dannyh@hotmail.com>
2014-06-05 10:39:51 -0700

    Initial load

---
+++ src/app_message.c
@@ -0,0 +1,89 @@
+#include <pebble.h>
+
+Window *window;	
+TextLayer *text_layer;
+	
+// Key values for AppMessage Dictionary
+enum {
+	STATUS_KEY = 0,	
+	MESSAGE_KEY = 1
+};
+
+// Write message to buffer & send
+void send_message(void){
+	DictionaryIterator *iter;
+	
+	app_message_outbox_begin(&iter);
+	dict_write_uint8(iter, STATUS_KEY, 0x1);
+	dict_write_cstring(iter, MESSAGE_KEY, "Hi Phone, I'm a Pebble!");
+
+	dict_write_end(iter);
+  	app_message_outbox_send();
+}
+
+// Called when a message is received from PebbleKitJS
+static void in_received_handler(DictionaryIterator *received, void *context) {
+	Tuple *tuple;
+	
+	tuple = dict_find(received, STATUS_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "WATCH Received Status: %d", (int)tuple->value->uint32); 
+	}
+	
+	tuple = dict_find(received, MESSAGE_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "WATCH Received Message: %s", tuple->value->cstring);
+    
+	// Set the text, font, and text alignment
+	text_layer_set_text(text_layer, tuple->value->cstring);
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	
+	// Add the text layer to the window
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+
+	// Push the window
+	window_stack_push(window, true);
+	
+	// App Logging!
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
+
+    
+    
+    
+	}}
+
+// Called when an incoming message from PebbleKitJS is dropped
+static void in_dropped_handler(AppMessageResult reason, void *context) {	
+}
+
+// Called when PebbleKitJS does not acknowledge receipt of a message
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+}
+
+void init(void) {
+	window = window_create();
+	window_stack_push(window, true);
+  text_layer = text_layer_create(GRect(0, 0, 144, 154));
+	
+	// Register AppMessage handlers
+	app_message_register_inbox_received(in_received_handler); 
+	app_message_register_inbox_dropped(in_dropped_handler); 
+	app_message_register_outbox_failed(out_failed_handler);
+		
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	
+	send_message();
+}
+
+void deinit(void) {
+	app_message_deregister_callbacks();
+  text_layer_destroy(text_layer);
+	window_destroy(window);
+}
+
+int main( void ) {
+	init();
+	app_event_loop();
+	deinit();
+}
